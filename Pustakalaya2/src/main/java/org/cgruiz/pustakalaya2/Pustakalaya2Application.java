d25cc21f6479d0472bef677a3b0b1f832f5d7f39
Cio-Ann <cgruiz79@gmail.com>
2019-04-19 14:05:27 +0200

    Commit inicial

---
+++ Pustakalaya2/src/main/java/org/cgruiz/pustakalaya2/Pustakalaya2Application.java
@@ -0,0 +1,13 @@
+package org.cgruiz.pustakalaya2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pustakalaya2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pustakalaya2Application.class, args);
+	}
+
+}
---
+++ Pustakalaya2/src/test/java/org/cgruiz/pustakalaya2/Pustakalaya2ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cgruiz.pustakalaya2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pustakalaya2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
